5542221200ba7300150ee8d86efedb4f42d679d0
Loïc Hoguin <essen@ninenines.eu>
2013-11-13 18:23:13 +0100

    Properly identify documentation type and project name

--- _build/src/docs.erl
+++ _build/src/docs.erl
@@ -7,33 +7,34 @@ build(Projects) ->
 
 project(P) ->
 	{_, Name} = lists:keyfind(name, 1, P),
+	{_, Title} = lists:keyfind(title, 1, P),
 	io:format("Building project: ~s~n", [Name]),
-	guide(Name),
-	manual(Name),
-	readme(Name),
+	guide(Name, Title),
+	manual(Name, Title),
+	readme(Name, Title),
 	io:format("Done.~n"),
 	ok.
 
 %% Guide and manual.
 
-guide(P) ->
+guide(P, T) ->
 	io:format("  ~s: guide~n", [P]),
-	build_dir(P, "/guide/").
+	build_dir(P, T, guide, "/guide/").
 
-manual(P) ->
+manual(P, T) ->
 	io:format("  ~s: manual~n", [P]),
-	build_dir(P, "/manual/").
+	build_dir(P, T, manual, "/manual/").
 
-build_dir(P, Suffix) ->
+build_dir(P, T, Type, Suffix) ->
 	Path = "deps/" ++ P ++ Suffix,
 	case file:list_dir(Path) of
 		{ok, Filenames} ->
-			[build_file(Path ++ F, P, F, Suffix) || F <- Filenames];
+			[build_file(Path ++ F, P, T, Type, F, Suffix) || F <- Filenames];
 		{error, enoent} ->
 			ok
 	end.
 
-build_file(Filename, P, F, Suffix) ->
+build_file(Filename, P, T, Type, F, Suffix) ->
 	case filename:extension(Filename) of
 		".md" ->
 			io:format("  ~s: down ~s~n", [P, Filename]),
@@ -42,9 +43,9 @@ build_file(Filename, P, F, Suffix) ->
 			Html = docs_parser:convert(Data),
 			{ok, Body} = docs_dtl:render([
 				{contents, Html},
-				{type, "idk"},
+				{type, Type},
 				{toc, ""},
-				{info, P}
+				{project, T}
 			]),
 			case filename:basename(F, ".md") of
 				"toc" ->
@@ -63,7 +64,7 @@ build_file(Filename, P, F, Suffix) ->
 
 %% README.
 
-readme(P) ->
+readme(P, T) ->
 	io:format("  ~s: readme~n", [P]),
 	Path = "deps/" ++ P ++ "/README.md",
 	{ok, Data} = file:read_file(Path),
@@ -73,7 +74,7 @@ readme(P) ->
 		{contents, Html},
 		{type, readme},
 		{toc, <<>>},
-		{info, P}
+		{project, T}
 	]),
 	filelib:ensure_dir(OutPath),
 	file:write_file(OutPath, Body).
